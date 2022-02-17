e66f58880bf8b7e0b415c1e53ec49e7604e82cc8
Abbi Engel <abbi.engel33@gmail.com>
2022-02-17 13:10:58 -0600

    error message

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
 class BankAccount {
-    constructor(start) {
-        this.balance = start;
-    }
+  constructor(start) {
+    this.balance = start;
+  }
 }
 
 class VendingMachine {
-    constructor() {
-        this.items = new Map();
-        this.items.set('Sprite', 1.25);
-        this.items.set('Coke', 1.50);
-        this.items.set('Water', 1.75);
-        this.items.set('Tea', 2.00);
-    }
+  constructor() {
+    this.items = new Map();
+    this.items.set("Sprite", 1.25);
+    this.items.set("Coke", 1.5);
+    this.items.set("Water", 1.75);
+    this.items.set("Tea", 2.0);
+  }
 
-    buy(account, item) {
-        const price = this.items.get(item);
+  buy(account, item) {
+    const price = this.items.get(item);
 
-        if (price == undefined) {
-            throw new Error('The item does not exist');
-        } 
-        if (price < account.balance) {
-            account.balance - price;
-                console.log(`Successfully bought ${item}.`)
-        } else {
-            throw new Error('Insufficient Funds.');
-        }
+    if (price == undefined) {
+      throw new Error("The item does not exist");
     }
+    if (price < account.balance) {
+      account.balance -= price;
+      console.log(`Successfully bought ${item}.`);
+    } else {
+      throw new Error("Insufficient Funds.");
+    }
+  }
 }
 
 class App {
-    static main() {
-        const account = new BankAccount(200);
-        const machine = new VendingMachine();
-
-        machine.buy(account, 'Sprite');
+  static main() {
+    const account = new BankAccount(200);
+    const machine = new VendingMachine();
+    try {
+      machine.buy(account, "Pepsi");
+    } catch (err) {
+      console.log("That item is not in our system!");
     }
+  }
 }
 
 App.main();
